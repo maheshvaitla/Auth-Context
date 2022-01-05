837905715ca0151290f2206e9b3d892926f52d10
maheshvaitla <maheshvaitla@gmail.com>
2022-01-05 09:58:52 +0530

    code added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import {Navbar} from './components/Navbar'
+function App() {
+  return (
+    <div className="App">
+      <h1>Auth</h1>
+      <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import {Login} from './Login'
+export const Navbar = () => {
+    return (
+         
+         <div>
+             <input type="text" placeholder="Enter Your name" /> 
+            <input type="submit" value="Submit" />
+            <Login/>
+         </div>
+     )
+ }
